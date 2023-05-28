8f8a635f126bc353d6a2baa1161f40044690b7b4
Rana Ebadah <ranaebadah@hotmail.com>
2023-05-28 23:51:31 +0400

    headerFile

---
+++ main.h
@@ -0,0 +1,17 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+#define BUFF_SIZE 1024
+#include<stdio.h>
+#include<stdarg.h>
+#include<unistd.h>
+
+
+int _putchar(char c);
+int printChar(va_list args);
+int _printf(const char *format, ...);
+
+
+
+
+#endif /* MAIN_H */
